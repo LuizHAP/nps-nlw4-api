a701ae655137259561fbfc49c95977dfca943837
Luiz Pansarini <lpansarini@gmail.com>
2021-02-25 15:09:52 -0300

    Aula 03 - Testando a nossa aplicação

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express'
 import { UserController } from './controllers/UserController';
+import { SurveyController } from './controllers/SurveyController';
 
 const router = Router();
 
 const usersController = new UserController();
+const surveysController = new SurveyController();
 
 router.post('/users', usersController.create);
+
+router.get('/surveys', surveysController.show);
+router.post('/surveys', surveysController.create);
 
 export { router }
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,3 @@
-import express from 'express'
-import "reflect-metadata"
-import "./database"
-import { router } from './router';
-
-const app = express();
-
-app.use(express.json());
-app.use(router);
+import { app } from "./app";
 
 app.listen(3333, () => console.log('Server is running'))
